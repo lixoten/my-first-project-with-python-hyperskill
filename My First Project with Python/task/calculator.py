f1cfc7d40a7edfbe012e2b1be3835f25d8d0753c
lixoten <lixoten@gmail.com>
2025-01-12 16:35:13 -0800

    Stage 2/3 Completed

--- My First Project with Python/task/calculator.py
+++ My First Project with Python/task/calculator.py
@@ -1,8 +1,13 @@
-# Stage 1/3
-print("Prices:")
-print("Bubblegum: $2")
-print("Toffee: $0.2")
-print("Ice cream: $5")
-print("Milk chocolate: $4")
-print("Doughnut: $2.5")
-print("Pancake: $3.2")
+# Stage 2/3
+print("Earned amount:")
+print("Bubblegum: $202")
+print("Toffee: $118")
+print("Ice cream: $2250")
+print("Milk chocolate: $1680")
+print("Doughnut: $1075")
+print("Pancake: $80")
+print()
+
+total = 202 + 118 + 2250 + 1680 + 1075 + 80
+
+print("Income: $", total, sep="")
