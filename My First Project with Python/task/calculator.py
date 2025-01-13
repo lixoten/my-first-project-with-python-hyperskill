319cf2f8c8377461bb1ea9c0c751e6f764972b48
lixoten <lixoten@gmail.com>
2025-01-12 18:48:22 -0800

    Stage 3/3 Completed

--- My First Project with Python/task/calculator.py
+++ My First Project with Python/task/calculator.py
@@ -1,4 +1,4 @@
-# Stage 2/3
+# Stage 3/3
 print("Earned amount:")
 print("Bubblegum: $202")
 print("Toffee: $118")
@@ -11,3 +11,11 @@ print()
 total = 202 + 118 + 2250 + 1680 + 1075 + 80
 
 print("Income: $", total, sep="")
+
+staffExpenses = int(input("Staff expenses:\n"))
+otherExpenses = int(input("Other expenses:\n"))
+
+netIncome = total - staffExpenses - otherExpenses
+
+print("Net Income: $", netIncome, sep="")
+
